c8ccaa1dfb4f83eae487edd119d57725bd1b1f17
vasu <vasutomar26@gmail.com>
2024-10-05 18:12:05 +0530

    enabled CORS

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ import express from 'express';
 import { authenticationRouter } from './routers/authentication-router.js';
 import { onboardingRouter } from './routers/onboarding-router.js';
 import { getVariable } from './config/getVariables.js';
-import bodyParser from 'body-parser';
-
 import { createLogger } from './utils/logger-utils.js';
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const logger = createLogger();
 
 const port = getVariable('PORT') | 3001;
 const host = getVariable('HOSTNAME') | '127.0.0.1';
 
-logger.info(`Fetched host : ${host} and port :${port}`);
-
 const app = express();
 app.set('port', port);
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use(authenticationRouter);
 app.use(onboardingRouter);
 
